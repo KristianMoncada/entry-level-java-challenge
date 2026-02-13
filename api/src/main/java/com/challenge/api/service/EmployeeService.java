5a0bb4fa1ffc7ee875a3d07bae6b2da84587ca13
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2026-02-13 13:13:43 -0500

    Implemented Employee methods

--- api/src/main/java/com/challenge/api/service/EmployeeService.java
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -1,14 +1,13 @@
 package com.challenge.api.service;
 
 // Needed to generate UUID
-import java.util.UUID;
-import java.util.List;
-import java.time.Instant;
-import java.util.ArrayList;
-import org.springframework.stereotype.Service;
-
 import com.challenge.api.model.Employee;
 import com.challenge.api.model.EmployeeImpl;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
 
 // Service layer that provides the business logic for the controller
 
@@ -23,8 +22,8 @@ public class EmployeeService {
 
         // Generates random UUID for each employee
         UUID uuid1 = UUID.randomUUID();
-        // UUID uuid2 = UUID.randomUUID();
-        
+        UUID uuid2 = UUID.randomUUID();
+
         employee1.setUuid(uuid1);
         employee1.setFirstName("Kristian");
         employee1.setLastName("Moncada");
@@ -37,6 +36,39 @@ public class EmployeeService {
         employee1.setContractTerminationDate(null);
         employees.add(employee1);
 
+        employee2.setUuid(uuid2);
+        employee2.setFirstName("John");
+        employee2.setLastName("Doe");
+        employee2.setFullName("John Doe");
+        employee2.setSalary(65000);
+        employee2.setAge(29);
+        employee2.setJobTitle("Team Manager");
+        employee2.setEmail("johndoe@outlook.com");
+        employee2.setContractHireDate(Instant.parse("2022-03-10T00:00:00Z"));
+        employee2.setContractTerminationDate(Instant.parse("2023-08-01T00:00:00Z"));
+        employees.add(employee2);
     }
+    // Returns list
+    public List<Employee> getAllEmployees() {
+        return employees;
+    }
+    // Returns uuid of employee if exists
+    public Employee getEmployeeByUuid(UUID uuid) {
+        for (Employee employee : employees) {
+            if (uuid.equals(employee.getUuid())) {
+                return employee;
+            }
+        }
 
+        return null;
+    }
+    // Creates Employee
+    public Employee createEmployee(Object requestBody) {
+        Employee newEmployee = new EmployeeImpl();
+        UUID uuid = UUID.randomUUID();
+
+        newEmployee.setUuid(uuid);
+
+        
+    }
 }
